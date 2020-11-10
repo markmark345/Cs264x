738e49d3ccb36bacf200cdc2feb8b73577be2c08
markmark345 <natapatchara.anuroje@gmail.com>
2020-11-10 19:49:46 +0700

    ทำเพิ่มเติม

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(express.static('public'));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.render("index", { fname: "gutohrs", lName: "hewgors" });
+  res.render("index", { });
 });
 
 app.listen(PORT, function () {
